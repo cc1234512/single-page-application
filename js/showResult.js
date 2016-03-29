aaa4a084b925e62f149eaa06cade546ec9884782
makkwun <cc1234512@gmail.com>
2016-03-29 09:08:50 +0000

    finished Remind user and Show user information

---
+++ js/showResult.js
@@ -0,0 +1,10 @@
+function scrollWin() {
+    window.scrollTo(0,800);
+}
+function showData() {
+    var email = document.getElementById("email").value;
+    var pw = document.getElementById("pass1").value;
+    document.getElementById('showEmail').innerHTML = "Eamil: " + email
+    document.getElementById('showPW').innerHTML = "Password: " + pw
+    document.getElementById("remindData").removeAttribute("hidden");
+}
